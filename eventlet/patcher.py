24ae1d863bb31b69fa6f877a2d2dec9f3cdfba65
John Eckersberg <jeckersb@redhat.com>
2023-12-11 10:14:26 -0500

    fixup

--- eventlet/patcher.py
+++ eventlet/patcher.py
@@ -519,8 +519,8 @@ class _PyRLock:
 
         """
         # This breaks locks created by _fix_py3_rlock, so we comment it out:
-        #if self._owner != get_ident():
-        #    raise RuntimeError("cannot release un-acquired lock")
+        # if self._owner != get_ident():
+        #     raise RuntimeError("cannot release un-acquired lock")
         self._count = count = self._count - 1
         if not count:
             self._owner = None
